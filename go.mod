ba4a2147f79a2d98deb7bcd78d49efb61f0cfd27
teddy.nutech <teddy.nutech@gmail.com>
2024-02-06 13:33:31 +0700

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/teddys48/Go-Microservices
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	BasicHandler()
+}
+
+func BasicHandler() {
+	fmt.Println("Hello World!")
+}
